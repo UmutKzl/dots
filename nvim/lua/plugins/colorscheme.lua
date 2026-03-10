606b9f770877c7f1b443b2f970c7d6bc18732db0
UmutKzl <umut.exe@icloud.com>
2026-03-10 20:26:32 +0300

    Update theme to Nightfox

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,11 @@
 return {
 	{
-		"ellisonleao/gruvbox.nvim",
+		--"ellisonleao/gruvbox.nvim",
+		"EdenEast/nightfox.nvim",
 		priority = 1000,
 		config = function()
-			vim.cmd([[colorscheme gruvbox]])
+			--vim.cmd([[colorscheme gruvbox]])
+			vim.cmd([[colorscheme nightfox]])
 		end,
 		opts = {},
 	},
