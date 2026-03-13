42630e41d9de37f6ab403db8f4dde10b7274e8a4
UmutKzl <umut.exe@icloud.com>
2026-03-13 22:20:23 +0300

    Add Leader + LG keymap to use LazyGit

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,5 +4,7 @@ vim.keymap.set("n", "<leader>bb", ":Telescope buffers <CR>", { desc = "List buff
 
 vim.keymap.set("n", "<leader>pv", ":Explore <CR>", { desc = "File explorer", silent = true })
 
+vim.keymap.set("n", "<leader>lg", ":LazyGit <CR>", { desc = "LazyGit", silent = true })
+
 vim.keymap.set("n", "<C-/>", ":ToggleTerm direction=float<CR>")
 vim.keymap.set("t", "<C-/>", "<cmd>ToggleTerm direction=float<CR>")
