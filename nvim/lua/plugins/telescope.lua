74a90c6ab918b4796327f906ad158d226b3d440c
UmutKzl <umut.exe@icloud.com>
2026-03-14 12:45:45 +0300

    Add Telescope.nvim

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,5 @@
+vim.pack.add({
+	"https://github.com/nvim-telescope/telescope.nvim",
+	"https://github.com/nvim-lua/plenary.nvim",
+	{ src = "https://github.com/nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+})
